fe8c3177b2a46e285c76d063d51485c3752655d7
diegogglez <diegomiller7@gmail.com>
2023-04-02 17:42:23 +0200

    Feat: game page added

---
+++ src/app/game/game.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.page.html',
+  styleUrls: ['./game.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class GamePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
